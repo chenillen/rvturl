cce30c3a2176d9a5a40b039d8543f2bdffdf7a11
chenillen <chenillen@gmail.com>
2010-09-16 18:11:54 +0800

    self generate slug

--- rvturl.rb
+++ rvturl.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 require 'active_record'
 
 # alphanumeric characters, case insensitive, stringified
-AVAILABLE_CHARACTERS = ( ('a'..'z').to_a + (0..9).to_a ).map {|char| char.to_s }
+AVAILABLE_CHARACTERS = ( ('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a ).map {|char| char.to_s }
 
 
 ## Configure of Database
@@ -36,11 +36,30 @@ end
 ## ActiveRecord Models
 class Url < ActiveRecord::Base
   has_many :clicks
+  before_validation :slug_generate
   validates :url, :presence => true, :uniqueness => true
-  # validates :slug, :presence => true, :uniqueness => true
+  validates :slug, :presence => true, :uniqueness => true
   
-  def self.slug_generate
-    # TODO
+
+  private
+  
+  def slug_generate
+    minimum_length ||= 4
+    if Url.find_by_slug(unique_slug(minimum_length))
+      minimum_length += 1
+      self.slug = unique_slug(minimum_length)
+    else
+      self.slug = unique_slug(minimum_length)
+    end
+  end 
+  
+  def unique_slug(leng)
+    length = AVAILABLE_CHARACTERS.size
+    slug = ""
+    1.upto(leng) do |i|
+      slug << AVAILABLE_CHARACTERS[rand(length-1)]
+    end
+    return slug
   end
 end
 
@@ -59,16 +78,21 @@ get "/" do
 end
 
 post "/shorten" do
-  @url = Url.new(:url => params[:url])
-  if @url.save
-    status(201) 
-    @url.to_json
-    "Shorten url generated"
+  @url = Url.find_or_initialize_by_url(params[:url])
+  if @url.new_record?
+    if @url.save
+      status(201) 
+      @url.to_json
+      "Shorten url generated"
+    else
+      "An error occurs"
+    end
   else
-    "An error occurs"
+    @url.to_json
   end
 end
 
 get "/:slug" do
-  
+  @url = Url.find_by_slug(params[:slug])
+  redirect @url.url
 end
